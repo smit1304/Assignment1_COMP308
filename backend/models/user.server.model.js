39512d47045576237605c894dd8f95b2775b819b
Sanjeev Chauhan <sanjeevchauhan3090@gmail.com>
2026-02-06 17:51:41 -0500

    Updated user.server.model.js
    - Updated Updated user.server.model.js to follow the ES module and have more security

--- backend/models/user.server.model.js
+++ backend/models/user.server.model.js
@@ -1,31 +1,54 @@
-// backend/models/user.server.model.js
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const bcrypt = require('bcryptjs');
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
-const UserSchema = new Schema({
-    username: { 
-        type: String, 
-        unique: true, 
+// Create user schema
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        unique: true,
         required: 'Username is required',
-        trim: true 
-    }, 
-    password: { 
-        type: String, 
-        required: 'Password is required' 
-    }, 
-    // Array of Game ObjectIDs for the user's collection [cite: 70, 71]
-    games: [{ 
-        type: Schema.Types.ObjectId, 
-        ref: 'Game' 
-    }]
+        trim: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: 'Password is required',
+        minlength: 6
+    },
+    // Reference to the Games collection
+    games: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Game'
+  }]
+}, {
+    timestamps: true // Automatically add createdAt and updatedAt fields
 });
 
 // Pre-save hook to hash passwords before saving to DB
-UserSchema.pre('save', async function() {
-    if (this.isModified('password')) {
-        this.password = await bcrypt.hash(this.password, 10);
+UserSchema.pre('save', async function(next) {
+    // only hash if the password is new or modified
+    if(!this.isModified('password')) return next();
+
+    try {
+        // Genreate salt for password hashing
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (err) {
+        next(err);
     }
 });
 
-mongoose.model('User', UserSchema);
+// Method to compare entered password with hashed password in DB
+UserSchema.methods.comparePassword = async function(userPassword) {
+    return await bcrypt.compare(userPassword, this.password);
+}
+
+// Hide sensitive fields when converting to JSON
+UserSchema.methods.toJSON = function() {
+    const obj = this.toObject();
+    delete obj.password; // Remove password
+    return obj;
+}
+
+export default mongoose.model('User', UserSchema);
